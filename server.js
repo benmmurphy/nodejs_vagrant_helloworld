38c6757270a4ad25ba54146d4af01de8599bb169
Ben Murphy <benmmurphy@gmail.com>
2012-03-04 12:28:53 +0000

    update server.js to work with new cluster

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var http = require('http');
 var express = require('express');
-
 var port = process.env.PORT == null ? 3000 : parseInt(process.env.PORT);
 if (isNaN(port)) {
   port = process.env.PORT;
@@ -10,21 +9,11 @@ var server = express.createServer(
     express.logger(':req[x-forwarded-for] - - [:date] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"')
 );
 
-server.on("close", function() {
-  process.exit(0);
-});
-
-server.on("listening", function() {
-  process.send("ncluster:ready");
-});
-
-process.on("SIGQUIT", function() {
-  server.close();
-});
-
 server.listen(port);
 
 server.get('/', function(req, res){
   res.send('Hello World from: ' + process.pid);
 });
+
+module.exports = server;
 
