56a83ae346392c1e67ef97a5856664df8c5b48a8
Ben Murphy <benmmurphy@gmail.com>
2012-03-07 20:45:00 +0000

    fix logging when not using nginx

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 var http = require('http');
 var express = require('express');
 var port = process.env.PORT == null ? 3000 : parseInt(process.env.PORT);
+var logger;
 if (isNaN(port)) {
   port = process.env.PORT;
+  logger = express.logger(':req[x-forwarded-for] - - [:date] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"');
+} else {
+  logger = express.logger();
 }
 
-var server = express.createServer(
-    express.logger(':req[x-forwarded-for] - - [:date] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"')
-);
+
+var server = express.createServer(logger);
 
 server.listen(port);
 
